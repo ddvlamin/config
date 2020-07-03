abee6952985e92a7cff6e769c1c8fd4322fac959
Dieter Devlaminck <ddvlamin@gmail.com>
2020-07-03 09:03:51 +0000

    configure cloud9 using pat

--- cloud9/configure_cloud9.sh
+++ cloud9/configure_cloud9.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-github_key=$1
+github_user=$1
+github_pat=$2
 
 pushd ~/environment/
+
+git config --global credential.helper store
 
 rm -Rf ~/.pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
@@ -12,24 +15,26 @@ cp config/cloud9/.bashrc ~/.bashrc
 source ~/.bashrc
 
 pyenv install 3.6.9
-pyenv virtualenv 3.6.9 data-utilities
-pyenv virtualenv 3.6.9 notebooks
-
-ssh-add $github_key
+pyenv virtualenv 3.6.9 biostrand
 
 cp -R config/cloud9/environment/.c9/* .c9/
 
 function create_repository () {
 	rm -Rf $1
 	mkdir $1
-	git clone "git@github.com:BioStrand/"$1".git" $1
+	git clone "https://"$github_user":"$github_pat"@github.com/BioStrand/"$1".git" $1
 	pushd $1
-	pyenv local $1
+	pyenv local biostrand
+	if [ -f setup.py ]; then
+		pip install -e .
+	fi
+	if [ ! -f setup.py ] && [ -f requirements.txt ]; then
+	       pip install -r requirements.txt
+       	fi	       
 	popd
 }
 
 create_repository data-utilities
-create_repository core
 create_repository notebooks
 
 popd
